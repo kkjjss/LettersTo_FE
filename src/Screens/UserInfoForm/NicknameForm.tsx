95fd103d093e4a1a159cb37f1e7bf7bceb2a4d1f
jjsskk <jinsukk9763@gmail.com>
2022-09-30 13:44:30 +0900

    닉네임 스크린 다음 버튼 활성화/비활성화 추가

--- src/Screens/UserInfoForm/NicknameForm.tsx
+++ src/Screens/UserInfoForm/NicknameForm.tsx
@@ -22,7 +22,7 @@ type Props = NativeStackScreenProps<StackParamsList, 'NicknameForm'>;
 export function NicknameForm({}: Props) {
   const [nickname, setNickname] = useState('');
   const [isDuplicate, setIsDuplicate] = useState(false);
-  const [alertText, setAlertText] = useState('');
+  const [isChecked, setIsChecked] = useState(false);
 
   const fadeAnim = useRef(new Animated.Value(0)).current;
 
@@ -42,25 +42,27 @@ export function NicknameForm({}: Props) {
     if (nickname) {
       alert.reset();
 
-      // api return
-
-      let res: boolean;
-
+      // api request
+      let response: boolean;
       if (nickname === 'A') {
-        res = true;
+        response = true;
       } else {
-        res = false;
+        response = false;
       }
 
-      if (res === true) {
+      if (response === true) {
         setIsDuplicate(false);
-        setAlertText('닉네임 사용이 가능해요.');
       } else {
         setIsDuplicate(true);
-        setAlertText('이미 사용 중인 닉네임이에요.');
       }
+      setIsChecked(true);
       alert.start();
     }
+  };
+
+  const changeNickname = (v: string) => {
+    setNickname(v);
+    setIsChecked(false);
   };
 
   return (
@@ -76,23 +78,33 @@ export function NicknameForm({}: Props) {
             <TextInput
               style={styles.nicknameInput}
               value={nickname}
-              onChangeText={setNickname}
+              onChangeText={changeNickname}
             />
             <View style={styles.checkButton}>
-              <Button title="중복확인" onPress={checkNicknameForDuplicates} />
+              <Button
+                title="중복확인"
+                disabled={!nickname}
+                onPress={checkNicknameForDuplicates}
+              />
             </View>
           </View>
         </View>
       </ScrollView>
       <Animated.View style={[styles.alert, {opacity: fadeAnim}]}>
-        <Text>{alertText}</Text>
+        {!isDuplicate ? (
+          <Text>닉네임 사용이 가능해요.</Text>
+        ) : (
+          <Text>이미 사용중인 닉네임이에요.</Text>
+        )}
       </Animated.View>
       <View style={{marginBottom: 30, marginTop: 10}}>
         <Button
           title="다음"
+          disabled={!(nickname && !isDuplicate && isChecked)}
           onPress={() => {
-            if (nickname && !isDuplicate)
+            if (nickname && !isDuplicate) {
               console.log('navigation.navigate(성향)');
+            }
           }}
         />
       </View>
