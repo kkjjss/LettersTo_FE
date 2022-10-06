53b5a7f69ca3bee19c90b0fe94a4d83bcb3a97a8
kkjjss <jinsukk9763@gmail.com>
2022-10-06 23:49:20 +0900

    그라데이션 적용

--- src/Screens/Auth/Auth.tsx
+++ src/Screens/Auth/Auth.tsx
@@ -1,9 +1,24 @@
 import * as React from 'react';
 import {NativeStackScreenProps} from '@react-navigation/native-stack';
-import {View, Text, Button, Dimensions, StyleSheet} from 'react-native';
+import {
+  View,
+  Text,
+  Button,
+  Dimensions,
+  StyleSheet,
+  TouchableWithoutFeedback,
+} from 'react-native';
 import type {StackParamsList} from '../../types';
+import {LinearGradient} from 'expo-linear-gradient';
 // import AsyncStorage from '@react-native-async-storage/async-storage';
 // import useStore from '../../Store/store';
+
+import {
+  login,
+  logout,
+  getProfile as getKakaoProfile,
+  unlink,
+} from '@react-native-seoul/kakao-login';
 
 type Props = NativeStackScreenProps<StackParamsList, 'Auth'>;
 
@@ -29,44 +44,84 @@ export function Auth({navigation}: Props) {
     }
   }
 
+  const signInWithKakao = async (): Promise<void> => {
+    try {
+      const token = await login();
+      console.log(token);
+    } catch (err) {
+      // eslint-disable-next-line no-console
+      console.error('login err', err);
+    }
+  };
+
   return (
-    <View style={styles.container}>
+    <LinearGradient
+      colors={['#ffccee', 'white', 'white', 'white', '#ffffcc']}
+      style={styles.container}>
       <View style={styles.titleWrapper}>
         <Text style={styles.titleText}>
           로그인 및 회원가입 1초 만에 시작하기
         </Text>
       </View>
       <View style={styles.buttonWrapper}>
-        <View style={styles.loginButton}>
-          <Button title="카카오로 시작하기" />
-        </View>
-        <View style={styles.loginButton}>
-          <Button title="네이버로 시작하기" />
-        </View>
-        <View style={styles.loginButton}>
-          <Button
-            title="Google로 시작하기"
-            onPress={() => signInWithSocialService('google')}
-          />
-        </View>
+        <TouchableWithoutFeedback>
+          <View style={[styles.loginButton, {backgroundColor: '#F9E54C'}]}>
+            <Text style={[styles.loginText]}>카카오로 시작하기</Text>
+          </View>
+        </TouchableWithoutFeedback>
+        <TouchableWithoutFeedback>
+          <View style={[styles.loginButton, {backgroundColor: '#03C75A'}]}>
+            <Text style={[styles.loginText]}>네이버로 시작하기</Text>
+          </View>
+        </TouchableWithoutFeedback>
+        <TouchableWithoutFeedback
+          onPress={() => signInWithSocialService('google')}>
+          <View
+            style={[
+              styles.loginButton,
+              // eslint-disable-next-line react-native/no-inline-styles
+              {
+                backgroundColor: '#ffffff',
+                borderWidth: 1,
+                borderColor: '#0000cc',
+              },
+            ]}>
+            <Text style={[styles.loginText]}>이메일로 시작하기</Text>
+          </View>
+        </TouchableWithoutFeedback>
       </View>
       <View style={styles.bottomWrapper}>
-        <Text>회원가입 시 개인정보처리방침을 읽었으며</Text>
-        <Text>이용약관에 동의하신 것으로 간주합니다</Text>
+        <Text style={styles.bottomText}>
+          회원가입 시 개인정보처리방침을 읽었으며
+        </Text>
+        <Text style={styles.bottomText}>
+          이용약관에 동의하신 것으로 간주합니다
+        </Text>
       </View>
-    </View>
+    </LinearGradient>
   );
 }
 
 const styles = StyleSheet.create({
   container: {flex: 1},
-  titleWrapper: {flex: 3, justifyContent: 'center', alignItems: 'center'},
-  titleText: {},
+  titleWrapper: {flex: 5, justifyContent: 'center', alignItems: 'center'},
+  titleText: {fontFamily: 'Galmuri11'},
   buttonWrapper: {
     flex: 3,
     justifyContent: 'center',
     alignItems: 'center',
   },
-  loginButton: {margin: 5, width: windowWidth * 0.8},
+  loginButton: {
+    width: 327,
+    height: 52,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginBottom: 12,
+  },
+  loginText: {
+    fontFamily: 'Galmuri11',
+    fontSize: 15,
+  },
   bottomWrapper: {flex: 1, alignItems: 'center'},
+  bottomText: {fontFamily: 'Galmuri11', fontSize: 12, color: '#0000cc'},
 });
