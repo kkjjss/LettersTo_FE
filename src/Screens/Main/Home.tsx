d00664b815e2b5924461cb9fe6c0e7f437b1094c
kkjjss <jinsukk9763@gmail.com>
2022-11-13 16:08:59 +0900

    편지 탐색 Ios 최적화

--- src/Screens/Main/Home.tsx
+++ src/Screens/Main/Home.tsx
@@ -1,15 +1,23 @@
-import React, { useRef, useState, useEffect } from 'react';
-import { NativeStackScreenProps } from '@react-navigation/native-stack';
-import type { StackParamsList } from '../../types/stackParamList';
+import React, {useRef, useState, useEffect} from 'react';
+import {NativeStackScreenProps} from '@react-navigation/native-stack';
+import type {StackParamsList} from '../../types/stackParamList';
 import useStore from '../../Store/store';
 import AsyncStorage from '@react-native-async-storage/async-storage';
-import { SafeAreaView } from 'react-native-safe-area-context';
-import { View, Text, StyleSheet, FlatList, VirtualizedList, TouchableOpacity, Image } from 'react-native';
-import { LinearGradient } from 'expo-linear-gradient';
-import { getPublicLetters } from '../../APIs/public';
-import { PublicLetters } from '../../types/types';
-import { PublicLetterItem } from './PublicLetterItem';
-import { EnvelopeModal } from '../../Modals/EnvelopeModal';
+import {SafeAreaView, useSafeAreaInsets} from 'react-native-safe-area-context';
+import {
+  View,
+  Text,
+  StyleSheet,
+  FlatList,
+  VirtualizedList,
+  TouchableOpacity,
+  Image,
+} from 'react-native';
+import {LinearGradient} from 'expo-linear-gradient';
+import {getPublicLetters} from '../../APIs/public';
+import {PublicLetters} from '../../types/types';
+import {PublicLetterItem} from './PublicLetterItem';
+import {EnvelopeModal} from '../../Modals/EnvelopeModal';
 
 type Props = NativeStackScreenProps<StackParamsList, 'Home'>;
 
@@ -30,10 +38,10 @@ export function Home({navigation}: Props) {
     PURPLE: 'rgba(170, 117, 255, 0.25)',
     LIGHTPURPLE: 'rgba(226, 168, 255, 0.25)',
   };
-  
+
   type StampType = {
     [key: number]: any;
-  }
+  };
   const STAMPS: StampType = {
     1: require('../../Assets/1062.jpg'),
     237: require('../../Assets/237.jpg'),
@@ -48,10 +56,10 @@ export function Home({navigation}: Props) {
   useEffect(() => {
     try {
       getPublicLetters().then(publicLettersData => {
-        const { content, cursor } = publicLettersData;
+        const {content, cursor} = publicLettersData;
         setPublicLetters(content);
         setCursor(cursor);
-      })
+      });
     } catch (error: any) {
       console.error(error.message);
     }
@@ -62,29 +70,29 @@ export function Home({navigation}: Props) {
   const handleScroll = (event: any) => {
     const positionY = event.nativeEvent.contentOffset.y;
     setPositionY(positionY);
-  }
+  };
 
   // 맨 상단으로 스크롤
   const publicLetterListRef = React.useRef<FlatList>(null);
   const scrollToTop = () => {
-    publicLetterListRef.current?.scrollToIndex({ animated: true, index: 0 });
-  }
+    publicLetterListRef.current?.scrollToIndex({animated: true, index: 0});
+  };
 
   // 무한 스크롤
   const handleEndReached = () => {
     if (cursor) {
       try {
         getPublicLetters(cursor).then(publicLettersData => {
-          const { content, cursor } = publicLettersData;
+          const {content, cursor} = publicLettersData;
           const updatedArray = [...publicLetters].concat(content);
           setPublicLetters(updatedArray);
           setCursor(cursor);
-        })
+        });
       } catch (error: any) {
         console.error(error.message);
       }
     }
-  }
+  };
 
   // 새로고침
   const [refreshing, setRefreshing] = useState(false);
@@ -92,15 +100,15 @@ export function Home({navigation}: Props) {
     setRefreshing(true);
     try {
       getPublicLetters().then(publicLettersData => {
-        const { content, cursor } = publicLettersData;
+        const {content, cursor} = publicLettersData;
         setPublicLetters(content);
         setCursor(cursor);
         setRefreshing(false);
-      })
+      });
     } catch (error: any) {
       console.error(error.message);
     }
-  }
+  };
 
   // 봉투 열기
   const [selectedItem, setSelectedItem] = useState({});
@@ -108,12 +116,12 @@ export function Home({navigation}: Props) {
   const onOpenPublicLetter = (item: any) => {
     setSelectedItem(item);
     setEnvelopeModalVisible(true);
-  }
+  };
 
   // 편지 조회
   const goToReadLetter = () => {
     navigation.navigate('ReadLetter');
-  }
+  };
 
   function logout() {
     AsyncStorage.removeItem('accessToken');
@@ -129,106 +137,153 @@ export function Home({navigation}: Props) {
     navigation.navigate('LetterEditor');
   }
 
-  return (
-    <LinearGradient locations={[0, 0.1, 0.8, 1]} colors={['#FFCCEE', '#FFFFFF', '#FFFFFF', '#FFFFCC']} style={styles.container}>
-      <SafeAreaView style={styles.container}>
-        {/* <Text>Home Screen</Text> */}
-        {/* <Button title="로그아웃" onPress={logout} /> */}
-        {/* <Button title="마이페이지" onPress={goToMyPage} /> */}
+  const {top: SAFE_AREA_TOP} = useSafeAreaInsets();
 
-        <LinearGradient colors={['rgba(255, 204, 238, 1)', 'rgba(255, 255, 255, 0)']} style={styles.header}>
-          <View style={styles.headerInner}>
-            <View style={{flexDirection: 'row'}}>
-              <TouchableOpacity>
-                <Image source={require('../../Assets/alert_off.png')} style={styles.icon} />
-              </TouchableOpacity>
-              <View style={{marginLeft: 12}}>
-                <Image source={require('../../Assets/alert_on.png')} style={styles.icon} />
-              </View>
-            </View>
-            <TouchableOpacity>
-              <Image source={require('../../Assets/menu.png')} style={styles.icon} />
+  return (
+    <LinearGradient
+      locations={[0, 0.1, 0.8, 1]}
+      colors={['#FFCCEE', '#FFFFFF', '#FFFFFF', '#FFFFCC']}
+      style={styles.container}>
+      {/* <SafeAreaView style={styles.container}> */}
+      <View style={[styles.header, {marginTop: SAFE_AREA_TOP}]}>
+        <View style={styles.headerInner}>
+          <View style={{flexDirection: 'row'}}>
+            <TouchableOpacity activeOpacity={0.7}>
+              <Image
+                source={require('../../Assets/alert_off.png')}
+                style={styles.icon}
+              />
             </TouchableOpacity>
+            <View style={{marginLeft: 12}}>
+              <Image
+                source={require('../../Assets/alert_on.png')}
+                style={styles.icon}
+              />
+            </View>
           </View>
-        </LinearGradient>
-        <FlatList
-          ref={publicLetterListRef}
-          onScroll={handleScroll}
-          onEndReached={handleEndReached}
-          onEndReachedThreshold={0.2}
-          refreshing={refreshing}
-          onRefresh={handleRefresh}
-          data={publicLetters}
-          keyExtractor={(item, index) => item.id}
-          renderItem={({item, index}) => {
-            const { id, title, fromNickname, fromAddress, topics, personalities, paperColor, stampId } = item;
-            const isFirst: boolean = index === 0;
-            const isLast: boolean = index === publicLetters.length - 1;
-            const cardAngle = [-5, 5, 5, -5, 15, 5];
-            return (
-              <View style={[
+          <TouchableOpacity activeOpacity={0.7}>
+            <Image
+              source={require('../../Assets/menu.png')}
+              style={styles.icon}
+            />
+          </TouchableOpacity>
+        </View>
+      </View>
+      <FlatList
+        ref={publicLetterListRef}
+        onScroll={handleScroll}
+        onEndReached={handleEndReached}
+        onEndReachedThreshold={0.2}
+        refreshing={refreshing}
+        onRefresh={handleRefresh}
+        data={publicLetters}
+        keyExtractor={(item, index) => item.id}
+        renderItem={({item, index}) => {
+          const {
+            id,
+            title,
+            fromNickname,
+            fromAddress,
+            topics,
+            personalities,
+            paperColor,
+            stampId,
+          } = item;
+          const isFirst: boolean = index === 0;
+          const isLast: boolean = index === publicLetters.length - 1;
+          const cardAngle = [-5, 5, 5, -5, 15, 5];
+          return (
+            <View
+              style={[
                 isFirst && {paddingTop: 116},
                 isLast && {paddingBottom: 60},
               ]}>
-                <PublicLetterItem
-                  id={id}
-                  title={title}
-                  fromNickname={fromNickname}
-                  fromAddress={fromAddress}
-                  topics={topics}
-                  personalities={personalities}
-                  paperColor={COLORS[paperColor]}
-                  stampSource={STAMPS[stampId]}
-                  onOpenPublicLetter={() => onOpenPublicLetter({
+              <PublicLetterItem
+                id={id}
+                title={title}
+                fromNickname={fromNickname}
+                fromAddress={fromAddress}
+                topics={topics}
+                personalities={personalities}
+                paperColor={COLORS[paperColor]}
+                stampSource={STAMPS[stampId]}
+                onOpenPublicLetter={() =>
+                  onOpenPublicLetter({
                     ...item,
                     paperColor: COLORS[paperColor],
                     stampSource: STAMPS[stampId],
-                  })}
-                  style={[
-                    index % 2 === 0 ? {left: '27.7%', marginTop: -36} : {left: '-6.4%', marginTop: -152},
-                    {transform: [{ rotate: `${cardAngle[index % 6]}deg` }]}
-                  ]}
-                />
-              </View>
-            )
-          }}
-        />
-        <View style={styles.tabBottom}>
-          <View style={styles.tabArea}>
-            <TouchableOpacity>
-              <View style={styles.tabActive}>
-                <Image source={require('../../Assets/triangle.png')} style={[styles.triangle, {right: '100%'}]} />
-                <Text style={styles.tabActiveText}>편지탐색</Text>
-              </View>
-            </TouchableOpacity>
-            <TouchableOpacity>
-              <View style={styles.tabInactive}>
-                <Image source={require('../../Assets/triangle.png')} style={[styles.triangle, {left: 0, transform: [{scaleX: -1}]}]} />
-                <Text style={styles.tabInactiveText}>내 사서함</Text>
-              </View>
-            </TouchableOpacity>
-          </View>
-        </View>
-        <View style={styles.floatArea}>
-          {positionY > 0
-            ? <TouchableOpacity style={[styles.btn, styles.btnPrimary]} onPress={scrollToTop}>
-                <Image source={require('../../Assets/top.png')} style={styles.icon} />
-              </TouchableOpacity>
-            : <TouchableOpacity style={[styles.btn, styles.btnPrimary]}>
-                <Image source={require('../../Assets/refresh.png')} style={styles.icon} />
-              </TouchableOpacity>
-          }
-          <TouchableOpacity style={[styles.btn, styles.btnSecondary]}>
-            <Image source={require('../../Assets/write.png')} style={styles.icon} />
+                  })
+                }
+                style={[
+                  index % 2 === 0
+                    ? {left: '27.7%', marginTop: -36}
+                    : {left: '-6.4%', marginTop: -152},
+                  {transform: [{rotate: `${cardAngle[index % 6]}deg`}]},
+                ]}
+              />
+            </View>
+          );
+        }}
+      />
+      <View style={styles.tabBottom}>
+        <View style={styles.tabArea}>
+          <TouchableOpacity activeOpacity={0.7}>
+            <View style={styles.tabActive}>
+              <Image
+                source={require('../../Assets/triangle.png')}
+                style={[styles.triangle, {right: '100%'}]}
+              />
+              <Text style={styles.tabActiveText}>편지탐색</Text>
+            </View>
+          </TouchableOpacity>
+          <TouchableOpacity activeOpacity={0.7}>
+            <View style={styles.tabInactive}>
+              <Image
+                source={require('../../Assets/triangle.png')}
+                style={[styles.triangle, {left: 0, transform: [{scaleX: -1}]}]}
+              />
+              <Text style={styles.tabInactiveText}>내 사서함</Text>
+            </View>
           </TouchableOpacity>
         </View>
-        <EnvelopeModal
-          letter={selectedItem}
-          isModalVisible={isEnvelopeModalVisible}
-          setModalVisible={setEnvelopeModalVisible}
-          onOpenLetter={goToReadLetter}
-        />
-      </SafeAreaView>
+      </View>
+      <View style={styles.floatArea}>
+        {positionY > 0 ? (
+          <TouchableOpacity
+            activeOpacity={0.7}
+            style={[styles.btn, styles.btnPrimary]}
+            onPress={scrollToTop}>
+            <Image
+              source={require('../../Assets/top.png')}
+              style={styles.icon}
+            />
+          </TouchableOpacity>
+        ) : (
+          <TouchableOpacity
+            activeOpacity={0.7}
+            style={[styles.btn, styles.btnPrimary]}>
+            <Image
+              source={require('../../Assets/refresh.png')}
+              style={styles.icon}
+            />
+          </TouchableOpacity>
+        )}
+        <TouchableOpacity
+          activeOpacity={0.7}
+          style={[styles.btn, styles.btnSecondary]}>
+          <Image
+            source={require('../../Assets/write.png')}
+            style={styles.icon}
+          />
+        </TouchableOpacity>
+      </View>
+      <EnvelopeModal
+        letter={selectedItem}
+        isModalVisible={isEnvelopeModalVisible}
+        setModalVisible={setEnvelopeModalVisible}
+        onOpenLetter={goToReadLetter}
+      />
+      {/* </SafeAreaView> */}
     </LinearGradient>
   );
 }
@@ -236,17 +291,60 @@ export function Home({navigation}: Props) {
 const styles = StyleSheet.create({
   container: {flex: 1},
   header: {position: 'absolute', zIndex: 10, width: '100%'},
-  headerInner: {height: 52, flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center', paddingHorizontal: 16},
+  headerInner: {
+    height: 52,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    paddingHorizontal: 16,
+  },
   tabBottom: {width: '100%', height: 37, backgroundColor: '#0000CC'},
-  tabArea: {position: 'absolute', bottom: '100%', flexDirection: 'row', width: '100%', justifyContent: 'center', alignItems: 'flex-end'},
-  tabActive: {width: 164, height: 45, alignItems: 'center', justifyContent: 'center', backgroundColor: '#0000CC', borderTopLeftRadius: 10, borderTopRightRadius: 10},
-  tabInactive: {width: 164, height: 38, alignItems: 'center', justifyContent: 'center', backgroundColor: '#FFFFFF', borderWidth: 1, borderBottomWidth: 0, borderColor: '#0000CC', borderTopLeftRadius: 10, borderTopRightRadius: 10},
-  tabActiveText: {fontFamily: 'Galmuri11', fontSize: 15, color:'#FFFFFF'},
-  tabInactiveText: {fontFamily: 'Galmuri11', fontSize: 15, color:'#0000CC'},
+  tabArea: {
+    position: 'absolute',
+    bottom: '100%',
+    flexDirection: 'row',
+    width: '100%',
+    justifyContent: 'center',
+    alignItems: 'flex-end',
+  },
+  tabActive: {
+    width: 164,
+    height: 45,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#0000CC',
+    borderTopLeftRadius: 10,
+    borderTopRightRadius: 10,
+  },
+  tabInactive: {
+    width: 164,
+    height: 38,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#FFFFFF',
+    borderWidth: 1,
+    borderBottomWidth: 0,
+    borderColor: '#0000CC',
+    borderTopLeftRadius: 10,
+    borderTopRightRadius: 10,
+  },
+  tabActiveText: {fontFamily: 'Galmuri11', fontSize: 15, color: '#FFFFFF'},
+  tabInactiveText: {fontFamily: 'Galmuri11', fontSize: 15, color: '#0000CC'},
   floatArea: {position: 'absolute', right: 24, bottom: 100},
-  btn: {width: 48, height: 48, alignItems: 'center', justifyContent: 'center', marginTop: 8, borderRadius: 24},
+  btn: {
+    width: 48,
+    height: 48,
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginTop: 8,
+    borderRadius: 24,
+  },
   btnPrimary: {backgroundColor: '#0000CC'},
-  btnSecondary: {backgroundColor: '#FFFFCC', borderWidth: 1, borderColor: '#0000CC'},
+  btnSecondary: {
+    backgroundColor: '#FFFFCC',
+    borderWidth: 1,
+    borderColor: '#0000CC',
+  },
   triangle: {position: 'absolute', bottom: 0, width: 4, height: 5},
-  icon: {width: 28, height: 28}
+  icon: {width: 28, height: 28},
 });
--- src/Screens/Main/PublicLetterItem.tsx
+++ src/Screens/Main/PublicLetterItem.tsx
@@ -1,39 +1,82 @@
-import { StyleSheet, Text, Pressable, View, Image, ImageBackground } from 'react-native';
-import { PublicLetter } from '../../types/types';
-import { LinearGradient } from 'expo-linear-gradient';
+import React from 'react';
+import {
+  StyleSheet,
+  Text,
+  Pressable,
+  View,
+  Image,
+  ImageBackground,
+} from 'react-native';
+import {PublicLetter} from '../../types/types';
+import {LinearGradient} from 'expo-linear-gradient';
 
 interface PublicLetterItemProps extends PublicLetter {
   stampSource: any;
   style?: object;
   onOpenPublicLetter: () => void;
-};
+}
 
 export function PublicLetterItem(props: PublicLetterItemProps) {
-  const { title, fromNickname, fromAddress, topics, personalities, stampSource, paperColor, style, onOpenPublicLetter } = props;
-  
+  const {
+    title,
+    fromNickname,
+    fromAddress,
+    topics,
+    personalities,
+    stampSource,
+    paperColor,
+    style,
+    onOpenPublicLetter,
+  } = props;
+
   return (
-    <Pressable style={[styles.publicLetterItem, style]} onPress={onOpenPublicLetter}>
-      <LinearGradient locations={[0, 0.5]} colors={[paperColor, 'white']} style={styles.background}>
+    <Pressable
+      style={[styles.publicLetterItem, style]}
+      onPress={onOpenPublicLetter}>
+      <LinearGradient
+        locations={[0, 0.5]}
+        colors={[paperColor, 'white']}
+        style={styles.background}>
         <Text style={styles.title}>⌜{title || '무제'}⌟︎︎</Text>
         <View style={styles.fromArea}>
-          <Image style={styles.fromImg} source={require('../../Assets/from.png')} />
-          <Text style={styles.fromText}>{`${fromNickname}, ${fromAddress}`}</Text>
+          <Image
+            style={styles.fromImg}
+            source={require('../../Assets/from.png')}
+          />
+          <Text
+            style={styles.fromText}>{`${fromNickname}, ${fromAddress}`}</Text>
         </View>
-        <ImageBackground source={require('../../Assets/bg_stamp.png')} style={styles.stampArea}>
+        <ImageBackground
+          source={require('../../Assets/bg_stamp.png')}
+          style={styles.stampArea}>
           <Image style={styles.stampImg} source={stampSource} />
         </ImageBackground>
         <View style={styles.tagArea}>
           <View style={styles.tagList}>
-            {topics?.filter((el: string, idx: number) => idx < 4).map((item, idx) => (
-              <Text key={idx} style={styles.tagItem}>{item}</Text>
-            ))}
-            {topics?.length > 4 && <Text style={styles.tagItem}>{`+${topics.length - 4}`}</Text>}
+            {topics
+              ?.filter((el: string, idx: number) => idx < 4)
+              .map((item, idx) => (
+                <Text key={idx} style={styles.tagItem}>
+                  {item}
+                </Text>
+              ))}
+            {topics?.length > 4 && (
+              <Text style={styles.tagItem}>{`+${topics.length - 4}`}</Text>
+            )}
           </View>
           <View style={styles.tagList}>
-            {personalities?.filter((el: string, idx: number) => idx < 4).map((item, idx) => (
-              <Text key={idx} style={styles.tagItem}>{item}</Text>
-            ))}
-            {personalities?.length > 4 && <Text style={styles.tagItem}>{`+${personalities.length - 4}`}</Text>}
+            {personalities
+              ?.filter((el: string, idx: number) => idx < 4)
+              .map((item, idx) => (
+                <Text key={idx} style={styles.tagItem}>
+                  {item}
+                </Text>
+              ))}
+            {personalities?.length > 4 && (
+              <Text style={styles.tagItem}>{`+${
+                personalities.length - 4
+              }`}</Text>
+            )}
           </View>
         </View>
       </LinearGradient>
@@ -42,15 +85,49 @@ export function PublicLetterItem(props: PublicLetterItemProps) {
 }
 
 const styles = StyleSheet.create({
-  publicLetterItem: {position: 'relative', overflow: 'hidden', width: '78.7%', height: 212, backgroundColor: 'white', borderWidth: 1, borderColor: '#0000CC', borderRadius: 10},
+  publicLetterItem: {
+    position: 'relative',
+    overflow: 'hidden',
+    width: '78.7%',
+    height: 212,
+    backgroundColor: 'white',
+    borderWidth: 1,
+    borderColor: '#0000CC',
+    borderRadius: 10,
+  },
   background: {flex: 1},
-  title: {width: 173, marginTop: 16, marginLeft: 16, fontFamily: 'Galmuri11', fontSize: 13, lineHeight: 23, color: '#0000CC'},
+  title: {
+    width: 173,
+    marginTop: 16,
+    marginLeft: 16,
+    fontFamily: 'Galmuri11',
+    fontSize: 13,
+    lineHeight: 23,
+    color: '#0000CC',
+  },
   fromArea: {position: 'absolute', bottom: 106, left: 16},
   fromImg: {width: 48, height: 22},
-  fromText: {marginLeft: 16, fontFamily: 'Galmuri11', fontSize: 12, color: '#0000CC'},
+  fromText: {
+    marginLeft: 16,
+    fontFamily: 'Galmuri11',
+    fontSize: 12,
+    color: '#0000CC',
+  },
   stampArea: {position: 'absolute', top: 16, right: 16, padding: 7},
   stampImg: {width: 60, height: 76},
   tagArea: {position: 'absolute', bottom: 16, right: 16, left: 16},
   tagList: {flexDirection: 'row', marginTop: 8},
-  tagItem: {height: 24, lineHeight: 24, paddingRight: 3, paddingLeft: 6, marginRight: 4, fontFamily: 'Galmuri11', fontSize: 12, color: '#0000CC', backgroundColor: 'rgba(0, 0, 204, 0.05)', borderWidth: 1, borderColor: '#0000CC'}
+  tagItem: {
+    height: 24,
+    lineHeight: 24,
+    paddingRight: 3,
+    paddingLeft: 6,
+    marginRight: 4,
+    fontFamily: 'Galmuri11',
+    fontSize: 12,
+    color: '#0000CC',
+    backgroundColor: 'rgba(0, 0, 204, 0.05)',
+    borderWidth: 1,
+    borderColor: '#0000CC',
+  },
 });
