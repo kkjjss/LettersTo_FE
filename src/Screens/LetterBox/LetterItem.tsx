c4c37471036b45841417ec121d17f9c9be6eedf7
imimbora <imimbora@gmail.com>
2022-11-26 02:16:04 +0900

    사서함 주고받은편지 목록 ing

--- src/Screens/LetterBox/LetterItem.tsx
+++ src/Screens/LetterBox/LetterItem.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useRef, useState, useEffect, useMemo} from 'react';
 import {
   StyleSheet,
   Text,
@@ -18,7 +18,7 @@ interface LetterItemProps {
 
 export function LetterItem(props: LetterItemProps) {
   const { data, style } = props;
-  const { paperColor, stampId, read, me, title } = data;
+  const { paperColor, stampId, read, me, title, deliveryType, deliveryDate, toAddress, fromAddress } = data;
 
   type StampType = {
       [key: number]: any;
@@ -30,6 +30,81 @@ export function LetterItem(props: LetterItemProps) {
       1056: require('../../Assets/1056.jpg'),
       1062: require('../../Assets/1062.jpg'),
   };
+
+  useEffect(() => {
+    console.log('data', data);
+  }, [data])
+
+  const IsArrived = useMemo(() => {
+    const arrivalDate = new Date(deliveryDate);
+    const today = new Date();
+    const result: boolean = today.getTime() > arrivalDate.getTime();
+    return result;
+  }, [deliveryDate]);
+  const DdayText = useMemo(() => {
+    const arrivalDate = new Date(deliveryDate);
+    const today = new Date();
+    const distance = arrivalDate.getTime() - today.getTime();
+    const days = Math.floor(distance/(1000*60*60*24));
+    const hours = Math.floor((distance % (1000*60*60*24))/(1000*60*60));
+    const minutes = Math.floor((distance % (1000*60*60))/(1000*60));
+    let result = ``;
+    if (days) result += `${days}일 `;
+    if (days > 0 || hours > 0) result += `${hours < 10 ? `0${hours}` : hours}시간 `;
+    result += `${minutes < 10 ? `0${minutes}` : minutes}분 후 도착`;
+    return result;
+}, [deliveryDate])
+
+  const ArrivedLetter = () => (
+    <Pressable style={[styles.letterItem]}>
+      <LinearGradient
+        locations={[0, 0.5]}
+        colors={[GRADIENT_COLORS[paperColor], 'white']}
+        style={[styles.background]}
+      >
+        <ImageBackground
+          source={require('../../Assets/bg_stamp.png')}
+          style={styles.stampArea}>
+          <Image style={styles.stampImg} source={STAMPS[stampId]} />
+          {deliveryType === 'STANDARD' ? (
+            <Image
+              source={require('../../Assets/StandardBox.png')}
+              style={{width: 46, height: 17, position: 'absolute', top: 7, left: 7}}
+            />
+          ) : deliveryType === 'EXPRESS' ? (
+            <Image
+              source={require('../../Assets/ExpressBox.png')}
+              style={{width: 46, height: 17, position: 'absolute', top: 7, left: 7}}
+            />
+          ) : null
+          }
+        </ImageBackground>
+        <Text style={styles.title}>⌜{title || '무제'}⌟︎︎</Text>
+      </LinearGradient>
+    </Pressable>
+  );
+  const PendingLetter = () => (
+    <Pressable style={[styles.letterItem]}>
+      <LinearGradient
+        locations={[0, 0.5]}
+        colors={[GRADIENT_COLORS[paperColor], 'white']}
+        style={[styles.background, {alignItems: 'center'}]}
+      >
+        <View style={styles.tooltipArea}>
+          <Text style={styles.tooltipText}>{me ? `${toAddress}으로 편지가 가고 있어요!` : `${fromAddress}에서 편지가 오고 있어요!`}</Text>
+          <Image
+            style={styles.tooltipTail}
+            source={require('../../Assets/tooltip.png')}
+          />
+        </View>
+        <Image
+          style={{width: 118, height: 24, marginTop: 16}}
+          source={require('../../Assets/pending.png')}
+        />
+        <Text style={{marginTop: 8, fontFamily: 'Galmuri11', fontSize: 11, color: '#0000CC'}}>{DdayText}</Text>
+      </LinearGradient>
+    </Pressable>
+  );
 
   return (
     <View style={[
@@ -49,20 +124,7 @@ export function LetterItem(props: LetterItemProps) {
           {!read && <View style={{position: 'absolute', top: 3, right: -2, width: 2, height: 2, backgroundColor: '#FF44CC'}} />}
         </View>
       </View>
-      <Pressable style={[styles.letterItem]}>
-        <LinearGradient
-          locations={[0, 0.5]}
-          colors={[GRADIENT_COLORS[paperColor], 'white']}
-          style={styles.background}
-        >
-          <ImageBackground
-            source={require('../../Assets/bg_stamp.png')}
-            style={styles.stampArea}>
-            <Image style={styles.stampImg} source={STAMPS[stampId]} />
-          </ImageBackground>
-          <Text style={styles.title}>⌜{title || '무제'}⌟︎︎</Text>
-        </LinearGradient>
-      </Pressable>
+      {IsArrived ? <ArrivedLetter /> : <PendingLetter /> }
     </View>
   );
 }
@@ -87,14 +149,17 @@ const styles = StyleSheet.create({
   },
   background: {flex: 1},
   title: {
-    width: 173,
+    width: '60%',
     marginTop: 16,
     marginLeft: 16,
     fontFamily: 'Galmuri11',
     fontSize: 13,
-    lineHeight: 23,
+    lineHeight: 24,
     color: '#0000CC',
   },
   stampArea: {position: 'absolute', top: 16, right: 16, padding: 7},
   stampImg: {width: 60, height: 76},
+  tooltipArea: {height: 32, marginTop: 47, paddingHorizontal: 10, backgroundColor: '#FFFFCC', borderWidth: 1, borderColor: '#0000CC', borderRadius: 5},
+  tooltipText: {fontFamily: 'Galmuri11', fontSize: 12, color: '#0000CC', lineHeight: 28},
+  tooltipTail: {position: 'absolute', top: '100%', left: '50%', width: 5, height: 4, marginLeft: -25},
 });
