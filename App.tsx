d2bb2d65167be807f4f754e438a30223b4a2a7fe
imimbora <imimbora@gmail.com>
2022-12-06 01:18:20 +0900

    하라는거 다 했는데

--- App.tsx
+++ App.tsx
@@ -1,10 +1,15 @@
 // In App.js in a new project
 
-import * as React from 'react';
+import React, {useEffect} from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import StackNavigator from './src/Navigator/Navigator';
+import SplashScreen from 'react-native-splash-screen';
 
 export default function App() {
+
+  useEffect(() => {
+    // SplashScreen.hide();
+  }, []);
   return (
     <NavigationContainer>
       <StackNavigator />
