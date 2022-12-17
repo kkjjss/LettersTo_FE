2321c6974824e66db2ddca48c7b308f17e177e76
DESKTOP-LFF2ENS\imimb <bora2116@naver.com>
2022-12-17 12:45:51 +0900

    수정이전으로

--- android/app/src/main/java/com/lettersto/MainActivity.java
+++ android/app/src/main/java/com/lettersto/MainActivity.java
@@ -6,7 +6,6 @@ import com.facebook.react.ReactActivityDelegate;
 import com.facebook.react.ReactRootView;
 
 import android.os.Bundle;
-import org.devio.rn.splashscreen.SplashScreen;
 
 public class MainActivity extends ReactActivity {
 
