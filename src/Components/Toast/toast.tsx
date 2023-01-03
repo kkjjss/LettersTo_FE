18e81339bdb79fb30bf0edbc5f557584f5696901
kkjjss <jinsukk9763@gmail.com>
2023-01-04 00:52:46 +0900

    토스트 메세지 추가

---
+++ src/Components/Toast/toast.tsx
@@ -0,0 +1,23 @@
+import Toast from 'react-native-root-toast';
+
+export const showToast = (
+  text: string,
+  duration = 2000,
+  position = -40,
+  shadow = true,
+  animation = true,
+  hideOnPress = true,
+  delay = 0,
+) => {
+  return Toast.show(text, {
+    duration,
+    position,
+    shadow,
+    animation,
+    hideOnPress,
+    delay,
+    textStyle: {
+      fontFamily: 'Galmuri11',
+    },
+  });
+};
