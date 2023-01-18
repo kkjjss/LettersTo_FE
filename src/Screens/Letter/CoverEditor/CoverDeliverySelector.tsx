abd97a9e2e438ef4cec12d3996460bcbe8ab2ba5
kkjjss <jinsukk9763@gmail.com>
2023-01-18 16:45:29 +0900

    개발 세팅

--- src/Screens/Letter/CoverEditor/CoverDeliverySelector.tsx
+++ src/Screens/Letter/CoverEditor/CoverDeliverySelector.tsx
@@ -1,6 +1,13 @@
 import {NativeStackScreenProps} from '@react-navigation/native-stack';
 import React, {useEffect, useMemo, useState} from 'react';
-import {Image, StyleSheet, Text, TouchableOpacity, View} from 'react-native';
+import {
+  Image,
+  ScrollView,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from 'react-native';
 import {StackParamsList} from '../../../types/stackParamList';
 import {useSafeAreaInsets} from 'react-native-safe-area-context';
 import {LetterCoverPreview} from '../../../Components/LetterEditor/LetterCoverPreview';
@@ -114,179 +121,181 @@ export function CoverDeliverySelector({navigation, route}: Props) {
             <Text style={styles.counter}>X {stampQuantity}</Text>
           </View>
         </View>
-        <View style={{flex: 1, padding: 24}}>
-          <TouchableOpacity
-            onPress={() => {
-              if (stampQuantity >= 1) {
-                setDeliveryType('STANDARD');
-              }
-            }}
-            style={[
-              {
-                width: '100%',
-                height: undefined,
-                aspectRatio: 327 / 132,
-                marginBottom: 20,
-                padding: 10,
-              },
-              deliveryType === 'STANDARD'
-                ? {
-                    borderWidth: 1,
-                    borderColor: '#0000cc',
-                    borderRadius: 10,
-                  }
-                : {
-                    borderWidth: 1,
-                    borderColor: '#7e7e7e',
-                    borderRadius: 10,
-                  },
-            ]}>
-            <View
+        <ScrollView>
+          <View style={{flex: 1, padding: 24}}>
+            <TouchableOpacity
+              onPress={() => {
+                if (stampQuantity >= 1) {
+                  setDeliveryType('STANDARD');
+                }
+              }}
               style={[
-                {flex: 1, padding: 10},
-                deliveryType === 'STANDARD' && {
-                  backgroundColor: 'rgb(239,239,251)',
+                {
+                  width: '100%',
+                  height: undefined,
+                  aspectRatio: 327 / 132,
+                  marginBottom: 20,
+                  padding: 10,
                 },
+                deliveryType === 'STANDARD'
+                  ? {
+                      borderWidth: 1,
+                      borderColor: '#0000cc',
+                      borderRadius: 10,
+                    }
+                  : {
+                      borderWidth: 1,
+                      borderColor: '#7e7e7e',
+                      borderRadius: 10,
+                    },
               ]}>
               <View
-                style={{
-                  flexDirection: 'row',
-                  alignItems: 'center',
-                  justifyContent: 'space-between',
-                  marginBottom: 8,
-                }}>
+                style={[
+                  {flex: 1, padding: 10},
+                  deliveryType === 'STANDARD' && {
+                    backgroundColor: 'rgb(239,239,251)',
+                  },
+                ]}>
+                <View
+                  style={{
+                    flexDirection: 'row',
+                    alignItems: 'center',
+                    justifyContent: 'space-between',
+                    marginBottom: 8,
+                  }}>
+                  <Text
+                    style={{
+                      fontFamily: 'Galmuri11-Bold',
+                      fontSize: 15,
+                      color: '#0000cc',
+                    }}>
+                    Standard
+                  </Text>
+                  <View
+                    style={{
+                      flexDirection: 'row',
+                      alignItems: 'center',
+                    }}>
+                    <Image
+                      source={require('../../../Assets/numberStamp.png')}
+                      style={{
+                        height: 20,
+                        width: 20,
+                        marginRight: 5,
+                      }}
+                    />
+                    <Text
+                      style={{
+                        fontFamily: 'Galmuri11',
+                        fontSize: 13,
+                        color: '#0000cc',
+                      }}>
+                      X 1
+                    </Text>
+                  </View>
+                </View>
+
                 <Text
                   style={{
-                    fontFamily: 'Galmuri11-Bold',
+                    fontFamily: 'Galmuri11',
                     fontSize: 15,
                     color: '#0000cc',
                   }}>
-                  Standard
+                  {standardDeliveryDate}후에 도착해요
                 </Text>
-                <View
-                  style={{
-                    flexDirection: 'row',
-                    alignItems: 'center',
-                  }}>
-                  <Image
-                    source={require('../../../Assets/numberStamp.png')}
-                    style={{
-                      height: 20,
-                      width: 20,
-                      marginRight: 5,
-                    }}
-                  />
-                  <Text
-                    style={{
-                      fontFamily: 'Galmuri11',
-                      fontSize: 13,
-                      color: '#0000cc',
-                    }}>
-                    X 1
-                  </Text>
-                </View>
               </View>
-
-              <Text
-                style={{
-                  fontFamily: 'Galmuri11',
-                  fontSize: 15,
-                  color: '#0000cc',
-                }}>
-                {standardDeliveryDate}후에 도착해요
-              </Text>
-            </View>
-          </TouchableOpacity>
-          <TouchableOpacity
-            onPress={() => {
-              if (stampQuantity >= 5) {
-                setDeliveryType('EXPRESS');
-              }
-            }}
-            style={[
-              {
-                width: '100%',
-                height: undefined,
-                aspectRatio: 327 / 132,
-                padding: 10,
-              },
-              deliveryType === 'EXPRESS'
-                ? {
-                    borderWidth: 1,
-                    borderColor: '#0000cc',
-                    borderRadius: 10,
-                  }
-                : {
-                    borderWidth: 1,
-                    borderColor: '#cccccc',
-                    borderRadius: 10,
-                  },
-            ]}>
-            <LinearGradient
-              colors={
+            </TouchableOpacity>
+            <TouchableOpacity
+              onPress={() => {
+                if (stampQuantity >= 5) {
+                  setDeliveryType('EXPRESS');
+                }
+              }}
+              style={[
+                {
+                  width: '100%',
+                  height: undefined,
+                  aspectRatio: 327 / 132,
+                  padding: 10,
+                },
                 deliveryType === 'EXPRESS'
-                  ? [
-                      '#FF47C119',
-                      '#FFFF0019',
-                      '#89F50019',
-                      '#44EFFF19',
-                      '#FF47C119',
-                    ]
-                  : ['white', 'white']
-              }
-              style={[{flex: 1, padding: 10}]}
-              start={{x: 0, y: 0}}
-              end={{x: 1, y: 1}}>
-              <View
-                style={{
-                  flexDirection: 'row',
-                  alignItems: 'center',
-                  justifyContent: 'space-between',
-                  marginBottom: 8,
-                }}>
-                <Image
-                  source={require('../../../Assets/Express.png')}
-                  style={{
-                    height: 28,
-                    width: 75,
-                    resizeMode: 'contain',
-                  }}
-                />
+                  ? {
+                      borderWidth: 1,
+                      borderColor: '#0000cc',
+                      borderRadius: 10,
+                    }
+                  : {
+                      borderWidth: 1,
+                      borderColor: '#cccccc',
+                      borderRadius: 10,
+                    },
+              ]}>
+              <LinearGradient
+                colors={
+                  deliveryType === 'EXPRESS'
+                    ? [
+                        '#FF47C119',
+                        '#FFFF0019',
+                        '#89F50019',
+                        '#44EFFF19',
+                        '#FF47C119',
+                      ]
+                    : ['white', 'white']
+                }
+                style={[{flex: 1, padding: 10}]}
+                start={{x: 0, y: 0}}
+                end={{x: 1, y: 1}}>
                 <View
                   style={{
                     flexDirection: 'row',
                     alignItems: 'center',
+                    justifyContent: 'space-between',
+                    marginBottom: 8,
                   }}>
                   <Image
-                    source={require('../../../Assets/numberStamp.png')}
+                    source={require('../../../Assets/Express.png')}
                     style={{
-                      height: 20,
-                      width: 20,
-                      marginRight: 5,
+                      height: 28,
+                      width: 75,
+                      resizeMode: 'contain',
                     }}
                   />
-                  <Text
+                  <View
                     style={{
-                      fontFamily: 'Galmuri11',
-                      fontSize: 13,
-                      color: '#0000cc',
+                      flexDirection: 'row',
+                      alignItems: 'center',
                     }}>
-                    X 5
-                  </Text>
+                    <Image
+                      source={require('../../../Assets/numberStamp.png')}
+                      style={{
+                        height: 20,
+                        width: 20,
+                        marginRight: 5,
+                      }}
+                    />
+                    <Text
+                      style={{
+                        fontFamily: 'Galmuri11',
+                        fontSize: 13,
+                        color: '#0000cc',
+                      }}>
+                      X 5
+                    </Text>
+                  </View>
                 </View>
-              </View>
 
-              <Text
-                style={{
-                  fontFamily: 'Galmuri11',
-                  fontSize: 15,
-                  color: '#0000cc',
-                }}>
-                바로 도착해요!
-              </Text>
-            </LinearGradient>
-          </TouchableOpacity>
-        </View>
+                <Text
+                  style={{
+                    fontFamily: 'Galmuri11',
+                    fontSize: 15,
+                    color: '#0000cc',
+                  }}>
+                  바로 도착해요!
+                </Text>
+              </LinearGradient>
+            </TouchableOpacity>
+          </View>
+        </ScrollView>
       </View>
     </View>
   );
